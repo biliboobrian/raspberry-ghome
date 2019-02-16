73b23010aeea0c4888b10cfb61a96615792b6abf
Pierre Hanauer <biliboobrian@yahoo.fr>
2019-02-16 10:58:29 +0100

    try playvideo

---
+++ yt.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import pafy
+import vlc
+
+url = "https://www.youtube.com/watch?v=bMt47wvK6u0"
+video = pafy.new(url)
+best = video.getbestaudio()
+playurl = best.url
+
+Instance = vlc.Instance()
+player = Instance.media_player_new()
+Media = Instance.media_new(playurl)
+Media.get_mrl()
+player.set_media(Media)
+player.play()
